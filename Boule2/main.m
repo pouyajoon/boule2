e395be5ab2720a3e00a1668317a8417c5a222e3f
Aurelien Gasser <aurelien.gasser@gmail.com>
2012-01-07 21:59:40 +0100

    Initial Commit

---
+++ Boule2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Boule2
+//
+//  Created by Aurelien Gasser on 1/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
